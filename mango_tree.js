950f2b1bb984ef444d0e9053a4741e4d5dac3d5b
Ahmad Fajar <shin.fajar@gmail.com>
2017-07-03 19:31:47 +0700

    release 1

--- mango_tree.js
+++ mango_tree.js
@@ -99,11 +99,112 @@ class Mango {
   */
 let mangoTree = new MangoTree()
 
-console.log('\nThe tree is alive! :smile:');
+console.log('\nThe mango tree is alive! :smile:');
 do {
   mangoTree.grow();
   mangoTree.produceMangoes();
   mangoTree.harvest();
   console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.height} | Fruits harvested = ${mangoTree.harvested}`)
 } while (mangoTree.healtyStatus != false);
-console.log('The tree has met its end. :sad:\n');
+console.log('The mango tree has met its end. :sad:\n');
+
+//  --------------- end of Release 0 ---------------
+
+// Release 1
+
+class AppleTree {
+  constructor() {
+    this._age = 0;
+    this._fruits = [];
+    this._harvested = '';
+    this._health = true;
+    this._height = 0;
+    this._maxAge = 25;
+    this._maxGrowAge = 14;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  get fruits() {
+    return this._fruits;
+  }
+
+  get harvested() {
+    return this._harvested;
+  }
+
+  get healtyStatus() {
+    if (this._age >= this._maxAge) return false;
+    else return true;
+  }
+
+  grow() {
+    let growthRate = (Math.ceil(Math.random() * 20) + 10) / 100; // random 0.1 - 0.3
+    this._age++;
+
+    if (this._age <= this._maxGrowAge) {
+      if (this._height === 0) {
+        this._height += 1 + growthRate;
+        this._height = this._height.toFixed(2);
+      } else {
+        this._height *= (1 + growthRate);
+        this._height = this._height.toFixed(2);
+      }
+    }
+  }  // ----- grow
+
+  produceApples() {
+    let produced = Math.round(Math.random() * 40) + 10; // 10 - 50
+    for (var i = 0; i <= produced ; i++) {
+      this._fruits[i] = new Apple();
+    }
+  }
+
+  harvest() {
+    let good = 0;
+    let bad = 0;
+
+    for (var i = 0; i <= this._fruits.length - 1; i++) {
+      let qual = this._fruits[i]._quality;
+      if (qual === 'good') good++;
+      else bad++;
+    }
+
+    this._harvested = `${good + bad} (${good} good & ${bad} bad)`;
+    this._fruits = [];
+  }
+
+}  // --------------- class AppleTree ---------------
+
+class Apple {
+  constructor() {
+    this._quality = this.quality()
+  }
+
+  quality() {
+    let qual = () => {
+      let rand = Math.round(Math.random());
+      return (rand === 1) ? 'good' : 'bad';
+    }
+    return this._quality = qual();
+  }
+}  // --------------- class Apple ---------------
+
+let appleTree = new AppleTree()
+
+console.log('\nThe apple tree is alive! :smile:');
+do {
+  appleTree.grow();
+  appleTree.produceApples();
+  appleTree.harvest();
+  console.log(`[Year ${appleTree.age} Report] Height = ${appleTree.height} | Fruits harvested = ${appleTree.harvested}`)
+} while (appleTree.healtyStatus != false);
+console.log('The apple tree has met its end. :sad:\n');
+
+//  --------------- end of Release 1 ---------------
